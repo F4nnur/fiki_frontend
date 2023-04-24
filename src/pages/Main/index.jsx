e7c12115a9e2cf994c1e47c602b005aa7569bebc
Fannur <Fann1kF@yandex.ru>
2023-04-25 00:18:18 +0300

    + resume

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import s from "./style.module.scss";
 
 const MainPage = () => {
-    // let re = [{t:'12', d:12}, {t:'12', d:12}, {t:'12', d:12}]
+    let re = [
+        {t: '12', d: 'dsahadadshdhhshsdahsdaaaaaaasahshshsahsahsahsahsahshshsahasjaadsadjajdjasdjdjsadjajdjssjsjdsjajdjsajdjajds'},
+        {t: '12', d: "dsahadadshdhhshsdahsdaaaaaaasahshshsahsahsahsahsahshshsahasjaadsadjajdjasdjdjsadjajdjssjsjdsjajdjsajdjajds"},
+        {t: '12', d: "dsahadadshdhhshsdahsdaaaaaaasahshshsahsahsahsahsahshshsahasjaadsadjajdjasdjdjsadjajdjssjsjdsjajdjsajdjajds"}
+    ]
     return (
-        <div className={s.form__mainpage}>
-            <div className={s.form__wrapper}>
-                <div className={s.form__wrapper_tittle}>
-                    <p>Заголовок</p>
-                </div>
-                <div className={s.form__wrapper_description}>
-                    <p className={s.form__wrapper_text}>воааыаоыаыаооываыаоыоаыаоыоаоыоаыоаоыаыаыоаоаааааааааааааааааааааааааааааааааааааааааааааааааа</p>
-                </div>
-                <div>
-                    <button>
-                        Посмотреть
-                    </button>
-                </div>
-            </div>
-        </div>
+        <>
+            {
+                re.map(item =>
+                    <div className={s.form__mainpage}>
+                        <div className={s.form__wrapper}>
+                            <div className={s.form__wrapper_tittle}>
+                                <p>{item.t}</p>
+                            </div>
+                            <div className={s.form__wrapper_description}>
+                                <p className={s.form__wrapper_text}>{item.d}</p>
+                            </div>
+                            <div>
+                                <button>
+                                    Посмотреть
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     );
 };
 export default React.memo(MainPage);
